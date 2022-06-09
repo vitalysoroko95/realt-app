42aa7a5076a177203bac85df40985a206fc40245
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2022-06-09 12:50:09 +0300

    fix: fix alt img

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -55,6 +55,7 @@ export default function ImageScrollbar({ data }) {
             width={1000}
             height={500}
             sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+            alt="image"
           />
         </Box>
       ))}
